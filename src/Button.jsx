fdcd7f27847b010f1c759707b6e93f35d6440675
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2024-08-01 18:22:47 +0200

    Create header

---
+++ src/Button.jsx
@@ -0,0 +1,9 @@
+﻿function Button({ children }) {
+  return (
+    <div className="btn">
+      <span className="btn-text">{children}</span>
+    </div>
+  );
+}
+
+export default Button;
---
+++ src/Header.jsx
@@ -0,0 +1,58 @@
+﻿import Button from './Button';
+
+function Header() {
+  return (
+    <div className="container">
+      <div className="box">
+        <header className="header-section">
+          <logo className="header-section-logo">
+            <img src="src/assets/logo.png" alt="Logo" />
+          </logo>
+
+          <div className="header-section-links">
+            <div className="header-section-link">
+              <a href="#" className="header-section-link-text">
+                Продукты
+              </a>
+              <img src="src/assets/Arrow.svg" alt="arrow" />
+            </div>
+            <div className="header-section-link">
+              <a href="#" className="header-section-link-text">
+                Платформа
+              </a>
+            </div>
+            <div className="header-section-link">
+              <a href="#" className="header-section-link-text">
+                Кейсы
+              </a>
+            </div>
+            <div className="header-section-link">
+              <a href="#" className="header-section-link-text">
+                Партнёрам
+              </a>
+            </div>
+            <div className="header-section-link">
+              <a href="#" className="header-section-link-text">
+                Блог
+              </a>
+            </div>
+            <div className="header-section-link">
+              <a href="#" className="header-section-link-text">
+                О компании
+              </a>
+            </div>
+            <div className="header-section-link">
+              <a href="#" className="header-section-link-text">
+                Контакты
+              </a>
+            </div>
+
+            <Button>Свяжитесь с нами</Button>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
